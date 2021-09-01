53d8f06683035d9101b010b0857f86f3c76bac0f
GitHub Action <action@github.com>
2021-09-01 12:04:21 +0000

    Storage Service commit hash: da2bf38

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Storage Service commit hash: 74b3c1b"
+    release_note="Storage Service commit hash: da2bf38"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
